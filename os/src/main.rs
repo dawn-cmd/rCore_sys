3ce164fc6131ef02a3b1ae43deb7f4b7942dd38a
HuaiYuan Jing <ls.hyjing@gmail.com>
2024-01-05 11:17:02 -0500

    Add function call ability

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,5 +1,22 @@
 #![no_std]
 #![no_main]
+
 mod lang_items;
 use core::arch::global_asm;
+
 global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        static mut sbss: usize;
+        static mut ebss: usize;
+    }
+    unsafe {
+        (sbss..ebss).for_each(|a| { (a as *mut u8).write_volatile(0) });
+    }
+}
